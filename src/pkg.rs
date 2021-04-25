2585fd9b1b8ed5b28999672f4e602ac40bbed4b7
imonace <mail@mona.moe>
2021-04-26 03:12:51 +0800

    finish query logic

--- src/pkg.rs
+++ src/pkg.rs
@@ -1,17 +1,19 @@
-
-use std::fmt;
 use std::error;
+use std::fmt;
 
-const OBS_API_URL: &str = "https://api.opensuse.org/";
+//const OBS_API_URL: &str = "https://api.opensuse.org/";
+const OBS_API_BASE: &str = r#"https://api.opensuse.org/search/published/binary/id?match=@name=""#;
+const OBS_API_REST: &str = r#"" and (contains-ic(@arch, "x86_64") or contains-ic(@arch, "noarch")) and not(contains-ic(@project, "home:")) and not(contains-ic(@name, "-debuginfo")) and not(contains-ic(@name, "-debugsource")) and not(contains-ic(@name, "-devel")) and not(contains-ic(@name, "-lang")) and contains-ic(@baseproject, "openSUSE")"#;
 
 pub fn get_pkg_version(pkgname: &str) -> String {
     if pkgname.is_empty() {
-        return "No pkgname provided".to_string();
+        "No pkgname provided.".to_string()
+    } else {
+        "Function under constructing!".to_string()
     }
-    format!("Function under constructing!")
 }
 
-async fn query_pkg() -> Result<String, Box<dyn error::Error>>{
+async fn query_pkg(pkgname: &str) -> Result<String, reqwest::Error> {
     let obs_username: String = match std::env::var("OBS_USERNAME") {
         Ok(t)  => t,
         Err(e) => panic!("OBS_USERNAME env variable not found. Error: {}", e),
@@ -20,21 +22,22 @@ async fn query_pkg() -> Result<String, Box<dyn error::Error>>{
         Ok(t)  => t,
         Err(e) => panic!("OBS_PASSWORD env variable not found. Error: {}", e),
     };
-    println!("{:?}", obs_username);
-    println!("{:?}", obs_password);
     let client = reqwest::Client::new();
-    let url: String = OBS_API_URL.parse().unwrap();
-
-    //client.get("https://httpbin.org/ip").basic_auth(obs_username, obs_password);
-    Ok(reqwest::get("https://httpbin.org/ip").await?.text().await?)
+    let url = format!("{}{}{}", OBS_API_BASE, pkgname, OBS_API_REST);
+    Ok(client.get(url).basic_auth(obs_username, Some(obs_password)).send().await?.text().await?)
 }
 
 
 #[tokio::test]
 async fn query_test() {
-    let result: String = match query_pkg().await{
+    let result: String = match query_pkg("neofetch").await{
         Ok(t) => t,
         Err(e) => panic!("Error: {}", e),
     };
-    println!("{:?}", result);
+    println!("{:?}", &result);
+    let result: String = match query_pkg("podman").await{
+        Ok(t) => t,
+        Err(e) => panic!("Error: {}", e),
+    };
+    println!("{:?}", &result);
 }
